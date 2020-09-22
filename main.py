d172cf370b1fd0d1cd132711e919b54d3275cb59
Carlos Varela <carlosjvarelaa@gmail.com>
2020-09-22 10:28:53 -0700

    Ahora realiza una suma

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def print_hi(name):
-    print(f'Hola, {name}')
+def suma ():
+    num1 = int(input('Ingrese un numero:'))
+    num2 = int(input('Ingrese otro numero:'))
+    print('La suma es: ', num1 + num2)
 
 if __name__ == '__main__':
-    print_hi('Carlos')
+    suma()
 
